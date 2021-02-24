096f4d17a96f230d26046bd8aa71fd37daf176e9
whereissam <sam135642@gmail.com>
2021-02-24 17:28:20 +0800

    update Cart emoty

--- src/views/Cart.vue
+++ src/views/Cart.vue
@@ -1,6 +1,15 @@
 <template>
-  <div class="cart">
-    <h1>This is an about page</h1>
+ <div class='w-full h-screen flex flex-nowrap bg-gray-600 bg-opacity-25'>
+
+  <div class='darks bg-gray-600 w-9/12' @click="$router.go(-1)"></div>
+
+  <div class='w-1/4 flex flex-wrap bg-gray-800' >
+    <h2 class='text-white w-full text-right mr-5 mt-5 ml-5 h-10 border-b-2 cursor-pointer' @click="$router.go(-1)">close</h2>
+    <div class='border-gray-800 w-full text-center'>
+      <div class='text-center self-start'>X</div>
+      No products in the cart.</div>
+    <p class='border-2 border-gray-500 border-opacity-50 w-full text-center h-14 align-middle m-5 md:h-7 self-end cursor-pointer' @click="$router.go(-1)">Continue shopping</p>
   </div>
+</div>
+  
 </template>
- 
